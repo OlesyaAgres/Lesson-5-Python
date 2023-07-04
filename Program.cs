3e93c6e43dbd5b934991825c0976f1497664df72
Olesya <olesyaspb.ru@mail.ru>
2023-07-04 09:43:25 +0300

    решения

--- Program.cs
+++ Program.cs
@@ -8,10 +8,20 @@
 
 // Решение
 
+/*
+a = int(input("Введите число "))
+b = int(input("Введите степень числа(целое неотрицательно число) "))
+
+def func(a, b):
+    if b == 0:
+        return 1
+
+    return a * func(a, b - 1)
 
 
+print(func(a, b))
 
-
+*/
 
 
 // Задача 28: Напишите рекурсивную функцию sum(a, b), 
@@ -26,4 +36,18 @@
     */
 
 // Решение
+/*
 
+a = int(input("Введите первое неотрицительное число "))
+b = int(input("Введите второе неотрицательно число "))
+
+
+def recursive_sum(a, b):
+    if a == 0:
+        return b
+    else:
+        return recursive_sum(a - 1, b + 1)
+
+
+print(recursive_sum(a, b))
+*/
